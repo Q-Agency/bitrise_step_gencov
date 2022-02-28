6e1c15bab1dbaa2a6e88daa61a3c6f64cbc341f3
zlatkomq <99484503+zlatkomq@users.noreply.github.com>
2022-02-28 16:06:19 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 set -ex
 
-app_url=$(curl -u "${browserstack_user_name}:${browserstack_access_key}" -X POST "https://api-cloud.browserstack.com/app-automate/upload" -F "file=@$file_to_upload")
+brew install lcov
+bash ./gencov.sh --withWidgetTests ${with_widgets}
+bash ./coverage_calc.sh --severity ${severity} --treshold ${treshold} --withWidgetTests ${with_widgets}
 
-echo $app_url
-  if [[ "$app_url" == *"app_url"* ]]
-  then
-    app_url=`echo $app_url | awk -F':' '{ print $2 ":" $3 }' | awk '{ print substr( $0, 2, length($0)-3 ) }'`
-    #app_url=`awk '{ print substr( $0, 1, length($0)-1 ) }'`
-    #app_url=${app_url%??}
-    echo $app_url
-    envman add --key BROWSERSTACK_APP_AUTOMATE_URL --value "$app_url"
-    exit 0
-  else
-    echo $app_url
-    echo "Error uploading file to Browserstack"
-    exit 1
-  fi
 #
 # --- Export Environment Variables for other Steps:
 # You can export Environment Variables for other Steps with
